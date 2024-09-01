1f1d968ace85725e2d0c3a4bec47b20c6137e56e
Евгений <40506054+commeta@users.noreply.github.com>
2024-09-02 01:15:37 +0300

    Update watch_dir.sh

--- watch_dir.sh
+++ watch_dir.sh
@@ -3,47 +3,54 @@
 WATCH_DIR="/var/log/httpd/modsec_audit/"
 PID_FILE="/var/run/watch_create.pid"
 
+# Check for running instances
 check_running() {
     if [ -f "$PID_FILE" ]; then
-        local pid=$(cat "$PID_FILE")
+        pid=$(cat "$PID_FILE")
+        
         if [ -d "/proc/$pid" ]; then
-            echo "Скрипт уже запущен с PID $pid"
-            exit 1
-        else
-            echo "Найден устаревший PID файл. Удаление..."
-            rm -f "$PID_FILE"
+			echo "Script already running with PID $pid. Sending SIGTERM."
+			stop_inotifywait
         fi
     fi
 }
 
+# Function to find and send SIGTERM to inotifywait process
+stop_inotifywait() {
+	ps aux | grep "inotifywait -m -r -e create --format %w%f $WATCH_DIR" | grep -v grep | awk '{print $2}'| while read pid; do
+		kill -SIGTERM "$pid"
+		echo "Sending SIGTERM to inotifywait process with PID $pid."
+	done
+}
+
+# Write current PID to file
 write_pid() {
-    echo $$ > "$PID_FILE"
+	echo $$ > "$PID_FILE"
 }
 
+# Cleanup on interrupt or termination
 cleanup() {
-    rm -f "$PID_FILE"
-    exit 0
+	rm -f "$PID_FILE"
+	stop_inotifywait
+	exit 0
 }
 
+# Trap signals for clean exit
 trap cleanup SIGINT SIGTERM
 
+# Check for existing instances and write PID
 check_running
 write_pid
 
-process_new_directory() {
-    local NEW_DIR=$1
-    chmod 770 "$NEW_DIR"
-    chown apache:fastsecure "$NEW_DIR"
-}
+# Monitor the audit directory for new files
+inotifywait -m -r -e create --format '%w%f' "$WATCH_DIR" | while read -r line; do
+	if [ -d "$line" ]; then
+		chmod 770 "$line"
+		chown apache:fastsecure "$line"
+	fi
+done
 
-(
-    while true; do
-        inotifywait -m -r -e create --format '%w%f' "$WATCH_DIR" | while read NEW_ITEM
-        do
-            if [ -d "$NEW_ITEM" ]; then
-                process_new_directory "$NEW_ITEM"
-            fi
-        done
-    done
-) &
+
+# Exit the script
 exit 0
+
