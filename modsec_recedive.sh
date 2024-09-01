64a073407308c6b1dc2986f9087858012d1195eb
Евгений <40506054+commeta@users.noreply.github.com>
2024-09-01 20:04:44 +0300

    Update modsec_recedive.sh

--- modsec_recedive.sh
+++ modsec_recedive.sh
@@ -11,29 +11,39 @@ ATTACK_THRESHOLD=10  # Number of attacks before logging to recedive file
 
 # Check for running instances
 check_running() {
-  if [ -f "$PID_FILE" ]; then
-    pid=$(cat "$PID_FILE")
-    # Проверяем, запущен ли процесс с указанным PID
-    if ps -p "$pid" > /dev/null 2>&1; then
-      echo "Скрипт уже запущен с PID $pid"
-      exit 1
-    else
-      # Удаляем устаревший PID-файл
-      echo "Найден устаревший PID-файл. Удаление..."
-      rm -f "$PID_FILE"
+    if [ -f "$PID_FILE" ]; then
+        pid=$(cat "$PID_FILE")
+        if [ -d "/proc/$pid" ]; then
+            echo "Script already running with PID $pid. Sending SIGTERM."
+			kill -SIGTERM "$pid"
+			stop_inotifywait
+            exit 0
+        else
+            # Found stale PID file, removing it
+            echo "Found stale PID file. Removing..."
+            rm -f "$PID_FILE"
+        fi
     fi
-  fi
+}
+
+# Function to find and send SIGTERM to inotifywait process
+stop_inotifywait() {
+	ps aux | grep "inotifywait -m -r -e create --format %w%f $WATCH_DIR" | grep -v grep | awk '{print $2}'| while read pid; do
+        kill -SIGTERM "$pid"
+        echo "Sending SIGTERM to inotifywait process with PID $pid."
+    done
 }
 
 # Write current PID to file
 write_pid() {
-  echo $$ > "$PID_FILE"
+	echo $$ > "$PID_FILE"
 }
 
 # Cleanup on interrupt or termination
 cleanup() {
-  rm -f "$PID_FILE"
-  exit 0
+	rm -f "$PID_FILE"
+	stop_inotifywait
+	exit 0
 }
 
 # Trap signals for clean exit
@@ -43,12 +53,6 @@ trap cleanup SIGINT SIGTERM
 check_running
 write_pid
 
-# Apache Mutex dir
-if [ ! -d "/var/run/mod_security" ]; then
-	mkdir -p "/var/run/mod_security"
-    chmod 770 "/var/run/mod_security"
-    chown apache:fastsecure "/var/run/mod_security"
-fi
 
 if [ ! -d $(dirname "$LOG_FILE") ]; then
 	mkdir -p $(dirname "$LOG_FILE")
@@ -57,73 +61,74 @@ fi
 
 # Ensure log files exist
 if [ ! -f "$LOG_FILE" ]; then
-  touch "$LOG_FILE"
+	touch "$LOG_FILE"
 fi
 
 if [ ! -f "$RECEDIVE_FILE" ]; then
-  touch "$RECEDIVE_FILE"
+	touch "$RECEDIVE_FILE"
 fi
 
 # Analyze ModSecurity audit log files
 analyze_log_file() {
-  local log_file="$1"
-  local ip_address
-  local error_count
-  local timestamp=$(date +%s)
-  local status_code
-  ip_address=$(grep "X-Real-IP" "$log_file" | awk '{print $2}')
-  status_code=$(grep "HTTP/1.1" "$log_file" | awk '{print $2}') 
-  if [[ $status_code -eq 403 ]]; then # Check for 403 status code
-    if grep -q "^$ip_address " "$LOG_FILE"; then
-      error_count=$(grep "^$ip_address " "$LOG_FILE" | awk '{print $2}')
-      # Проверка error_count
-      if [[ $error_count -lt $ATTACK_THRESHOLD ]]; then
-        error_count=$((error_count + 1))
-        sed -i "s/^$ip_address .*/$ip_address $error_count $timestamp/" "$LOG_FILE"
+	local log_file="$1"
+	local ip_address
+	local error_count
+	local timestamp=$(date +%s)
+	local status_code
+	ip_address=$(grep "X-Real-IP" "$log_file" | awk '{print $2}')
+	status_code=$(grep "HTTP/1.1" "$log_file" | awk '{print $2}') 
+	
+	if [[ $status_code -eq 403 ]]; then # Check for 403 status code
+		if grep -q "^$ip_address " "$LOG_FILE"; then
+			error_count=$(grep "^$ip_address " "$LOG_FILE" | awk '{print $2}')
+			# Проверка error_count
+			if [[ $error_count -lt $ATTACK_THRESHOLD ]]; then
+				error_count=$((error_count + 1))
+				sed -i "s/^$ip_address .*/$ip_address $error_count $timestamp/" "$LOG_FILE"
         
-        if [[ $error_count -eq $ATTACK_THRESHOLD ]]; then 
-            echo "$ip_address - $(date +'%Y-%m-%d %H:%M:%S')" >> "$RECEDIVE_FILE"
-        fi
-      else
-        # Получаем текущий timestamp из файла
-        timestamp=$(grep "^$ip_address " "$LOG_FILE" | awk '{print $3}')
-        error_count=$((error_count + 1))
-        sed -i "s/^$ip_address .*/$ip_address $error_count $timestamp/" "$LOG_FILE"
+				if [[ $error_count -eq $ATTACK_THRESHOLD ]]; then 
+					echo "$ip_address - $(date +'%Y-%m-%d %H:%M:%S')" >> "$RECEDIVE_FILE"
+				fi
+			else
+				# Получаем текущий timestamp из файла
+				timestamp=$(grep "^$ip_address " "$LOG_FILE" | awk '{print $3}')
+				error_count=$((error_count + 1))
+				sed -i "s/^$ip_address .*/$ip_address $error_count $timestamp/" "$LOG_FILE"
         
-        if [[ $((error_count % ATTACK_THRESHOLD)) -eq 0 ]]; then 
-            echo "$ip_address - $(date +'%Y-%m-%d %H:%M:%S')" >> "$RECEDIVE_FILE"
-        fi        
-      fi	
-    else
-      echo "$ip_address 1 $timestamp" >> "$LOG_FILE"
-    fi
-  fi
+				if [[ $((error_count % ATTACK_THRESHOLD)) -eq 0 ]]; then 
+					echo "$ip_address - $(date +'%Y-%m-%d %H:%M:%S')" >> "$RECEDIVE_FILE"
+				fi        
+			fi	
+		else
+			echo "$ip_address 1 $timestamp" >> "$LOG_FILE"
+		fi
+	fi
 }
 
 
 # Monitor the audit directory for new files
 inotifywait -m -r -e create --format '%w%f' "$WATCH_DIR" | while read -r line; do
-  if [ -f "$line" ]; then
-    analyze_log_file "$line"
+	if [ -f "$line" ]; then
+		analyze_log_file "$line"
     
-    # Очистка лога после обработки файла
-    current_timestamp=$(date +%s)
-    threshold_timestamp=$((current_timestamp - TIMEOUT))
-    # Перебираем все строки в файле
-    while read -r line; do
-      # Извлекаем timestamp из строки
-      timestamp=$(echo "$line" | awk '{print $3}')
-      # Проверяем, истек ли timestamp
-      if [[ $timestamp -lt $threshold_timestamp ]]; then
-        # Удаляем строку из файла
-        sed -i "/^$line$/d" "$LOG_FILE"
-      fi
-    done < "$LOG_FILE"    
-  else
-    chmod 770 "$line"
-    chown apache:fastsecure "$line"
-  fi
-done &
+		# Очистка лога после обработки файла
+		current_timestamp=$(date +%s)
+		threshold_timestamp=$((current_timestamp - TIMEOUT))
+		# Перебираем все строки в файле
+		while read -r line; do
+			# Извлекаем timestamp из строки
+			timestamp=$(echo "$line" | awk '{print $3}')
+			# Проверяем, истек ли timestamp
+			if [[ $timestamp -lt $threshold_timestamp ]]; then
+				# Удаляем строку из файла
+				sed -i "/^$line$/d" "$LOG_FILE"
+			fi
+		done < "$LOG_FILE"    
+	else
+		chmod 770 "$line"
+		chown apache:fastsecure "$line"
+	fi
+done
 
 
 # Exit the script
